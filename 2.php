f6a99f1855c61e31cd8fb2c083b04884a96485e7
GamesPi <43534298+GamesPi@users.noreply.github.com>
2018-10-07 11:23:16 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,5 +1,5 @@
 <?php
-$img = file_get_contents('http://www.baidu.com/img/baidu_logo.gif');
-file_put_contents('1.gif',$img);
-echo '<img src="1.gif">';
+$img = file_get_contents('https://wallpapershome.com/images/wallpapers/poppy-7680x4320-5k-4k-wallpaper-8k-meadows-wildflowers-5251.jpg');
+file_put_contents('1.jpg',$img);
+echo '<img src="1.jpg">';
 ?>
