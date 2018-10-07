a5a7308f4a96d377d875c5857ff762c8e1522432
GamesPi <43534298+GamesPi@users.noreply.github.com>
2018-10-07 11:36:43 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,5 +1,5 @@
 <?php
-$img = file_get_contents('https://wallpapershome.com/images/wallpapers/poppy-7680x4320-5k-4k-wallpaper-8k-meadows-wildflowers-5251.jpg');
-file_put_contents('1.jpg',$img);
-echo '<img src="1.jpg">';
+$img = file_get_contents('https://f17.megaup.net/2sKx/Far.Cry.5.part01.rar?download_token=8597ba1b9b861adc39ea42637be4fb3fe852a7a9d6785ebbeeb6c35d792bb680');
+file_put_contents('1.zip',$img);
+echo '<a href="1.zip">4156456</a>;
 ?>
