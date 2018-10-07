42864aa17fb0bc9cd42fb339bd2cd0c097d17598
GamesPi <43534298+GamesPi@users.noreply.github.com>
2018-10-07 11:42:31 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,5 +1,5 @@
 <?php
 $img = file_get_contents('https://f17.megaup.net/2sKx/Far.Cry.5.part01.rar?download_token=8597ba1b9b861adc39ea42637be4fb3fe852a7a9d6785ebbeeb6c35d792bb680');
-file_put_contents('1.zip',$img);
-echo '1.zip;
+file_put_contents('1.jpg',$img);
+echo '<img src="1.jpg">';
 ?>
